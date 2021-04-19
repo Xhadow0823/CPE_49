3271f8c3d851fd7ce782a52cd195a20f381795d0
Eric's MB <eric17618@gmail.com>
2021-04-19 15:51:02 +0800

    Create 16-10056.cpp

---
+++ 16-10056.cpp
@@ -0,0 +1,15 @@
+// https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=12&page=show_problem&problem=997
+#include <iostream>
+
+int main(void){
+
+
+    return 0;
+}
+/*
+2
+2 0.166666 1
+2 0.166666 2
+*/
+
+// https://kai-y.medium.com/uva-10056-what-is-the-probability-2985ac5d50cb
